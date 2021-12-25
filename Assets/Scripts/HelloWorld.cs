3ad5b139c3d67f91807d775961a11fc2766344e3
DESKTOP-VUJO6TH\DM G1050Ti <sotnikov8383@gmail.com>
2021-12-26 01:16:45 +0300

    HomeWork

--- Assets/Scripts/HelloWorld.cs
+++ Assets/Scripts/HelloWorld.cs
@@ -1,17 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HelloWorld : MonoBehaviour
 {
     public int min;
     public int max;
     private int _guess;
+    private int _couter;
+    public Text _text;
 
     void Start()
     {
-       
-        
+
+        _couter = 0;
 
         Debug.Log($"Загадай число от {min} до {max}");
         CalculateGeuss();
@@ -37,17 +40,22 @@ public class HelloWorld : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            Debug.Log($"Ура!!! Победа!!!");
+            _text.text = $"Ура!!! Победа!!! Понадобилось {_couter} ходов!";
+            min = 0;
+            max = 0;
+            _guess = 0;
+            _couter = 0;
 
         }
+      
 
     }
 
     private void CalculateGeuss()
     {
         _guess = (min + max) / 2;
-        Debug.Log($"Твое число {_guess}?Да?");
-        
+        _text.text = $"Твое число {_guess}?Да?";
+        _couter++;
     }
 
     
--- Assets/Scripts/UIText.cs
+++ Assets/Scripts/UIText.cs
@@ -1,18 +1,90 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIText : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Text myText;
+    public Text counter;
+    private int summa = 0;
+    private int turn = 0;
     void Start()
     {
+        myText.text = "Нажмите цифру от 1 до 9";
+    }
+
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1)||Input.GetKeyDown(KeyCode.Keypad1))
+        {
+          summa += 1;
+           Turn();
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2)||Input.GetKeyDown(KeyCode.Keypad2))
+        {
+       
+            summa += 2;
+            Turn();
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3)||Input.GetKeyDown(KeyCode.Keypad3))
+        {
+            summa += 3;
+           Turn();
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4)||Input.GetKeyDown(KeyCode.Keypad4))
+        {
+            summa += 4;
+            Turn();
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha5)||Input.GetKeyDown(KeyCode.Keypad5))
+        {
+            summa += 5;
+            Turn();
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha6)||Input.GetKeyDown(KeyCode.Keypad6))
+        {
+            summa += 6;
+            Turn();
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha7)||Input.GetKeyDown(KeyCode.Keypad7))
+        {
+            summa += 7;
+           Turn();
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha8)||Input.GetKeyDown(KeyCode.Keypad8))
+        {
+            summa += 8;
+            Turn();
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha9)||Input.GetKeyDown(KeyCode.Keypad9))
+        {
+            summa += 9;
+            Turn();
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            myText.text = "Нажмите цифру от 1 до 9";
+            summa = 0;
+            counter.text = $"Сумма {summa}";
+        }
+
+        if (summa >= 50)
+        {
+            myText.text = $"Поздравляю, ты победил за {turn} ходов";
+        }
+        
+        
+        
+        
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Turn()
     {
-        
+        counter.text = $"Сумма {summa}";
+        turn++;
     }
 }
