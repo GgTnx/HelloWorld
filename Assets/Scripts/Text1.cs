022c2fbd67dbb666552aa7905c8b7fc881929218
DESKTOP-VUJO6TH\DM G1050Ti <sotnikov8383@gmail.com>
2021-12-25 18:13:50 +0300

    no message

---
+++ Assets/Scripts/Text1.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Text : MonoBehaviour
+{
+    public Text _Text;
+    void Start()
+    {
+        _Text._Text = "Valenki";
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
